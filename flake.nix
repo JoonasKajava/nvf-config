4b046b9dae56689123019602f23138c3aa8f6215
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-03-29 12:12:36 +0200

    feat: flake setup

--- flake.nix
+++ flake.nix
@@ -1,15 +1,38 @@
 {
-  description = "A very basic flake";
+  description = "A nvf configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nvf.url = "github:notashelf/nvf";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      perSystem = {system, ...}: let
+        nvim =
+          (nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+              ./config
+            ];
+          })
+          .neovim;
+      in {
+        packages = {
+          default = nvim;
+        };
+      };
+    };
 }
