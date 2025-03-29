599e480934506a6369247127dda1491df90cc53b
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-03-29 13:21:11 +0200

    feat: dashboard draft

--- config/ui/dashboard-nvim.nix
+++ config/ui/dashboard-nvim.nix
@@ -1,4 +1,5 @@
 {
+  # TODO: Maybe replace with alpha-nvim
   vim.dashboard.dashboard-nvim = {
     enable = true;
     setupOpts = {
@@ -7,8 +8,44 @@
         statusline = false;
       };
       config = {
-        center = {
+        # TODO: Add some padding
+        week_header = {
+          enable = true;
         };
+        center = [
+          {
+            action = ":Telescope find_files"; # TODO: Replace with fzf-lua
+            desc = " Find File";
+            icon = " ";
+            key = "f";
+          }
+          {
+            action = "ene | startinsert";
+            desc = " New File";
+            icon = " ";
+            key = "n";
+          }
+          {
+            action = ":Telescope oldfiles";
+            desc = " Recent Files";
+            icon = " ";
+            key = "r";
+          }
+          {
+            action = ":Telescope live_grep";
+            desc = " Find Text";
+            icon = " ";
+            key = "g";
+          }
+          {
+            action = ":qa<CR>";
+            desc = " Quit";
+            icon = " ";
+            key = "q";
+          }
+        ];
+
+        vertical_center = true;
       };
     };
   };
