ba7b2fc71aae4a1e80fce8b91af980966f488c90
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-04-14 17:57:32 +0300

    feat: trying to add nixd

--- config/languages/nix.nix
+++ config/languages/nix.nix
@@ -1,11 +1,29 @@
-{
+{lib, ...}: let
+  inherit (lib.nvim.dag) entryBefore;
+  inherit (lib) mkIf;
+  use-nixd = false;
+in {
   vim.languages.nix = {
     enable = true;
+
     lsp = {
-      #server = "nixd"; use nil_ls for now
+      server = mkIf use-nixd "nixd";
       options = {
-        # TODO: add nixd options
       };
     };
+  };
+
+  vim.luaConfigRC = mkIf use-nixd {
+    find-flake-func =
+      entryBefore ["nix-lsp"]
+      /*
+      lua
+      */
+      ''
+        function find_root_flake()
+          local flake_root = vim.fs.root(0, "flake.nix")
+          return flake_root
+        end
+      '';
   };
 }
