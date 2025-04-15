42aae4bb285f7b80f9ba5f5cede5203023b292ec
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-04-15 21:19:25 +0300

    feat: add html lsp

--- config/languages/html.nix
+++ config/languages/html.nix
@@ -1,5 +1,21 @@
 {
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.nvim.dag) entryAfter;
+in {
   vim.languages.html = {
     enable = true;
   };
+  vim.luaConfigRC.html-lsp =
+    entryAfter ["lspconfig"]
+    /*
+    lua
+    */
+    ''
+      require("lspconfig").html.setup {
+        cmd = {"${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server", "--stdio"},
+      };
+    '';
 }
