542d07cec80f9ef402e07f51e7776c948f2c8617
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-06-04 00:05:30 +0300

    feat: otter will now auto activate

--- config/languages/otter-nvim.nix
+++ config/languages/otter-nvim.nix
@@ -1,5 +1,24 @@
-{
-  vim.lsp.otter-nvim = {
-    enable = true;
+{lib, ...}: let
+  inherit (lib.generators) mkLuaInline;
+in {
+  vim = rec {
+    lsp.otter-nvim = {
+      enable = true;
+    };
+    autocmds = lib.mkIf lsp.otter-nvim.enable [
+      {
+        enable = true;
+        desc = "Automatically activate Otter when opening a file";
+        event = ["Filetype"];
+        callback =
+          mkLuaInline
+          # lua
+          ''
+            function ()
+              require('otter').activate()
+            end
+          '';
+      }
+    ];
   };
 }
