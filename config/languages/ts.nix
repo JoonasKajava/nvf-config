1b9bbe6e700f2cda6349cd5dbc3f3ea0d0e28653
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-11-20 21:25:10 +0200

    fix: conform for javascript

--- config/languages/ts.nix
+++ config/languages/ts.nix
@@ -1,6 +1,11 @@
-{
+{config, ...}: {
   vim.languages.ts = {
     enable = true;
     extensions.ts-error-translator.enable = true;
+  };
+  vim.formatter.conform-nvim = {
+    setupOpts = {
+      formatters_by_ft.javascript = [config.vim.languages.ts.format.type];
+    };
   };
 }
