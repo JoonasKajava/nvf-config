5944cc54f8e663be8fb2a4c9c26f5e578186395d
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-01-05 10:13:59 +0200

    feat: upgrade

--- config/languages/rust.nix
+++ config/languages/rust.nix
@@ -1,53 +1,49 @@
-{
+{lib, ...}: let
+  inherit (lib.nvim.lua) toLuaObject;
+in {
   vim.languages.rust = {
     enable = true;
     extensions.crates-nvim.enable = true;
     # TODO: Crates options might be incomplete
     dap.enable = true;
 
-    lsp = {
-      opts =
-        /*
-        Lua
-        */
-        ''
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
-            },
-            -- Add clippy lints for Rust if using rust-analyzer
-            checkOnSave = true,
-            -- Enable diagnostics if using rust-analyzer
-            diagnostics = {
-              enable = true,
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-            files = {
-              excludeDirs = {
-                ".direnv",
-                ".git",
-                ".github",
-                ".gitlab",
-                "bin",
-                "node_modules",
-                "target",
-                "venv",
-                ".venv",
-              },
-            },
-          },
-        '';
+    lsp.opts = toLuaObject {
+      rust-analyzer = {
+        cargo = {
+          allFeatures = true;
+          loadOutDirsFromCheck = true;
+          buildScripts = {
+            enable = true;
+          };
+        };
+        # Add clippy lints for Rust if using rust-analyzer
+        checkOnSave = true;
+        # Enable diagnostics if using rust-analyzer
+        diagnostics = {
+          enable = true;
+        };
+        procMacro = {
+          enable = true;
+          ignored = {
+            async-trait = ["async_trait"];
+            napi-derive = ["napi"];
+            async-recursion = ["async_recursion"];
+          };
+        };
+        files = {
+          excludeDirs = [
+            ".direnv"
+            ".git"
+            ".github"
+            ".gitlab"
+            "bin"
+            "node_modules"
+            "target"
+            "venv"
+            ".venv"
+          ];
+        };
+      };
     };
   };
 }
