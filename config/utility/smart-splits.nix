b6a9ae3be47b235be88c7cb10d1d239cb2e534b0
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-02-05 12:42:29 +0200

    fix: fix find files speed

--- config/utility/smart-splits.nix
+++ config/utility/smart-splits.nix
@@ -7,6 +7,12 @@
         move_cursor_right = "<c-l>";
         move_cursor_down = "<c-j>";
         move_cursor_up = "<c-l>";
+
+        # TODO: Find better keymaps for these
+        swap_buf_left = null;
+        swap_buf_right = null;
+        swap_buf_up = null;
+        swap_buf_down = null;
       };
     };
   };
